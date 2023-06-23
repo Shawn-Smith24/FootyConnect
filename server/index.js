34363278984fd5c9138c98f738ec1920f3960f01
shawn <shawnsmith0024@gmail.com>
2023-06-23 11:11:19 -0400

    backend setup

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ const app = express();
 
 app.use('/posts', postsRoutes);
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://shawnsmith0024:Chelsea0327@footyconnect.q35lg5c.mongodb.net/?retryWrites=true&w=majority'
