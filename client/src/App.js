67ef7e717d3942db59c2b1734628fe1baf896b44
shawn <shawnsmith0024@gmail.com>
2023-06-23 11:30:26 -0400

    posts skeleton set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
 
+//Component imports
+import { Posts } from "./Components/Posts/Posts";
+import { Form } from "./Components/Form/Form";
+import FootyImg from './images/FootyImg.jpeg'
 
 const App = () => {
     return(
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Footy Connect</Typography>
+                <img src={FootyImg} alt="footy" height="400" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent = "space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={7}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
---
+++ client/src/Components/Form/Form.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export const Form = () => {
+    return(
+        <h1>
+            Form
+        </h1>
+    )
+}
