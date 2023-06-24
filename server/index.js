11351fdec37238b5fde390b7be9a1c5c7c2386e5
shawn <shawnsmith0024@gmail.com>
2023-06-24 09:06:00 -0400

    redux set up, starting post set up

--- server/index.js
+++ server/index.js
@@ -8,11 +8,13 @@ import postsRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postsRoutes);
+
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/posts', postsRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://shawnsmith0024:Chelsea0327@footyconnect.q35lg5c.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT|| 5555;
